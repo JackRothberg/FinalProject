5cc1a3889e545b5b8f53d1fb48666bdedf738c6c
JackRothberg <jack_rothberg@hotmail.com>
2018-04-30 21:56:19 -0400

    Part of the loader class

--- src/project/Loader.java
+++ src/project/Loader.java
@@ -1,10 +1,28 @@
 package project;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
 
 public class Loader {
-	public static String load(MachineModel model, File file, int codeOffset, int memoryOffset) {
-		int codeSize = 0;
-		return "";
-	}
+	
+//	public static String load(MachineModel model, File file, int codeOffset, int memoryOffset) {
+//		int codeSize = 0;
+//		if (model == null || file == null) {
+//			return null;
+//		}
+//		try (Scanner input = new Scanner(file)) {
+//			boolean incode = true;
+//			while(input.hasNextLine()) {
+//			
+//			}
+//		} catch (ArrayIndexOutOfBoundsException e) {
+//			return "Array Index " + e.getMessage();
+//		} catch (NoSuchElementException e) {
+//			return "From Scanner: NoSuchElementException";
+//		} catch (FileNotFoundException e) {
+//			return "File " + file.getName() + " Not Found";
+//		}
+//	}
 }
